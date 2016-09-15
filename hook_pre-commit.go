5f9232eb9c8ace34ed12eb83b5d5da1e43f12af7
M. Adam Kendall <michael.kendall@gsa.gov>
2016-09-15 15:11:38 -0400

    Refactor check

--- hook_pre-commit.go
+++ hook_pre-commit.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/apuigsech/seekret-source-git"
-	"runtime"
 )
 
 func HookPreCommitEnable(args []string) (string, error) {
@@ -20,27 +18,11 @@ func HookPreCommitRun(args []string) error {
 		"staged": true,
 	}
 
-	err := gs.seekret.LoadObjects(sourcegit.SourceTypeGit, gs.repo, options)
+	secrets, err := gs.RunCheck(options)
 	if err != nil {
 		return err
 	}
-
-	gs.seekret.Inspect(runtime.NumCPU())
-
-	listSecrets := gs.seekret.ListSecrets()
-	fmt.Printf("Found Secrets: %d\n", len(listSecrets))
-	for _, s := range listSecrets {
-		fmt.Printf("\t%s:%d\n", s.Object.Name, s.Nline)
-		fmt.Printf("\t\t- Metadata:\n")
-		for k, v := range s.Object.Metadata {
-			fmt.Printf("\t\t  %s: %s\n", k, v)
-		}
-		fmt.Printf("\t\t- Rule:\n\t\t  %s\n", s.Rule.Name)
-
-		fmt.Printf("\t\t- Content:\n\t\t  %s\n", s.Line)
-	}
-
-	if len(listSecrets) > 0 {
+	if secrets != 0 {
 		return fmt.Errorf("commit cannot proceed")
 	}
 
--- main_check.go
+++ main_check.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/apuigsech/seekret-source-git"
 	"github.com/libgit2/git2go"
 	"github.com/urfave/cli"
-	"runtime"
 )
 
 func GitSeekretCheck(c *cli.Context) error {
@@ -31,24 +29,12 @@ func GitSeekretCheck(c *cli.Context) error {
 		options["staged"] = true
 	}
 
-	err = gs.seekret.LoadObjects(sourcegit.SourceTypeGit, gs.repo, options)
+	secrets, err := gs.RunCheck(options)
 	if err != nil {
 		return err
 	}
-
-	gs.seekret.Inspect(runtime.NumCPU())
-
-	listSecrets := gs.seekret.ListSecrets()
-	fmt.Printf("Found Secrets: %d\n", len(listSecrets))
-	for _, s := range listSecrets {
-		fmt.Printf("\t%s:%d\n", s.Object.Name, s.Nline)
-		fmt.Printf("\t\t- Metadata:\n")
-		for k, v := range s.Object.Metadata {
-			fmt.Printf("\t\t  %s: %s\n", k, v)
-		}
-		fmt.Printf("\t\t- Rule:\n\t\t  %s\n", s.Rule.Name)
-
-		fmt.Printf("\t\t- Content:\n\t\t  %s\n", s.Line)
+	if secrets != 0 {
+		return fmt.Errorf("Please remove discovered secrets")
 	}
 
 	return nil
